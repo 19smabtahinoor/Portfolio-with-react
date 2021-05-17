2faf156660fdb53c16ef616ad9676ac8c218b01a
S.M.ABTAHI NOOR <73340940+19smabtahinoor@users.noreply.github.com>
2021-05-17 21:19:32 +0600

    Version 1.7
    Fix small Errors

--- src/components/ProjectsData.jsx
+++ src/components/ProjectsData.jsx
@@ -1,7 +1,7 @@
 const Projectdata = [
     {
         id:1,
-        image: 'https://lh6.googleusercontent.com/or0Ym4Qb5FZDYT1ybNgaz7A4CtaQ17Bp2guACX-xeJABd7l-yAsRJwJegBJ7bfd9Z9t88f-1s1Qk-_WcL06tbquUnmrvBQz_yPTaMuRg3_xIPlQE7InM5WQYyKPJyw=w1280',
+        image: 'https://i.ibb.co/Wzd6bLN/fnsclub.png',
         projectTitle: 'Fns Club Clone',
         projectTools: 'HTML,CSS,Javascript,FontAwesome,Google Fonts',
         liveDemo: 'https://fnsclubbyabtahinoor.netlify.app/',
@@ -9,7 +9,7 @@ const Projectdata = [
     },
     {
         id:2,
-        image: 'https://lh5.googleusercontent.com/ih86EuJavbZIlvJ3oQAVgR7R61yY7SQ2OXDf41BMD6xw78vHyVYjMA78kVDRiLqOP_cWnkvoN7bUXmFlqvEJN5s7tncyenRdiOkU6fDhymT3Mgvy3rLP0vrhG0brJe_DJA=w1280',
+        image: 'https://i.ibb.co/NK9p93K/restaurant.jpg',
         projectTitle: 'Restaurant Landing Page',
         projectTools: 'HTML,CSS,Javascript,FontAwesome,Google Fonts',
         liveDemo: 'https://lambertrestaurant.netlify.app/',
@@ -17,7 +17,7 @@ const Projectdata = [
     },
     {
         id:3,
-        image: 'https://lh4.googleusercontent.com/VPI1jk7r328vjCl99xEfTQJWAEz79jt4GizTMx1YvnSlUdu382YUz1QnDSFBZgPknN8gnDi42G6w0AFDVio8ikgoRey3KdOwPVq6JKnx9B0lBvM4XXnX4gUrPlE8_PDVIw=w1280',
+        image: 'https://i.ibb.co/WtKVzyn/construction.png',
         projectTitle: 'Construction Landing Page',
         projectTools: 'HTML,CSS,Javascript,FontAwesome,Google Fonts',
         liveDemo: 'https://brigatebyabtahi.netlify.app/',
@@ -25,7 +25,7 @@ const Projectdata = [
     },
     {
         id:4,
-        image: 'https://lh4.googleusercontent.com/x8GjYLtiH6l3FsNQS8GDkWYNV0TJRoVZGc7eU1wbS023xzq30A-ETWW9Lh1ho3g1JTZSCzVyn2hquIj9pPBnMx11-cNDSVZ5GWC8wqzm2XXeh0AEsuwED0NKix4-TQ7ZKw=w1280',
+        image: 'https://i.ibb.co/v3LG0yF/box.png',
         projectTitle: 'Box Hover Effect',
         projectTools: 'HTML,CSS',
         liveDemo: 'https://creativehoverabtahi.netlify.app/',
@@ -33,7 +33,7 @@ const Projectdata = [
     },
     {
         id:5,
-        image: 'https://lh4.googleusercontent.com/M3WNVTkiG8uYL8AE1lsiMC-G1KfwOacAJila6FkE4rGAvGxpyxWjFgYQ37n1O18V2QU73Q8zuTLwIS6X_0LCD-2v2OatO9sNSOTtle7Gp4eVkvOoxttBobzGYATGLEtnig=w1280',
+        image: 'https://i.ibb.co/bzbRSk5/button.png',
         projectTitle: 'Creative Button Hover Effect',
         projectTools: 'HTML,CSS',
         liveDemo: 'https://buttonhovereffect.netlify.app/',
@@ -41,7 +41,7 @@ const Projectdata = [
     },
     {
         id:6,
-        image: 'https://lh5.googleusercontent.com/BZoXKmMOQ2owZOTyQqtNY7K7CFIoG-V-X4cXJV088kdBgt4DUAueUFbg0uPHUKK4RSvMJXhLhLLW4dx-MtDU8wKeqwiEKClZjq6Hm5fHfDgmgx79nbsi2uyax6leTArCJA=w1280',
+        image: 'https://i.ibb.co/JkyWNLm/expending.jpg',
         projectTitle: 'Expanding Image Cards',
         projectTools: 'HTML,CSS,Javascript',
         liveDemo: 'https://expanding-image-cards.vercel.app/',
@@ -49,7 +49,7 @@ const Projectdata = [
     },
     {
         id:7,
-        image : 'https://lh5.googleusercontent.com/0ez_k_Fnyz0nZYn4vg_KYfHN5j-xUB8M_-ILsbZSMTyuOIYxlL4ihUorfQ-joJz7MRa34mt7hWKZnPsyhzeZznOoitNABpMo7h1ic58UcbnInAXVTKJ_w5h9lmwxin8eNg=w1280',
+        image : 'https://i.ibb.co/cQCnBX8/gkeep.png',
         projectTitle:'Google Keep Clone',
         projectTools:'HTML,CSS,React',
         liveDemo:'https://google-keep-clone-react.vercel.app/',
@@ -57,7 +57,7 @@ const Projectdata = [
     },
     {
         id:8,
-        image : 'https://lh5.googleusercontent.com/6x82XewILLcXKscyM0xx1ZLN6pUaMoTQOBwqBRZEnGilzYfVlCXajX4qdwdOb4yUWL5oRctJrjvGUu-VPe0g9k9IgCvblSv8GkdPJ98mgih-sCbMLwxgPAnFyESEUzyCBA=w1280',
+        image : 'https://i.ibb.co/HBmqkbZ/imagsearch.png',
         projectTitle:'Live Image Search',
         projectTools:'HTML,CSS,React',
         liveDemo:'https://live-image-search-react.vercel.app',
@@ -65,7 +65,7 @@ const Projectdata = [
     },
     {
         id:9,
-        image : 'https://lh5.googleusercontent.com/nMcZPe9etPRM1J_teifo0xHoyrC3oCmnZd8CVhuetqwjuaP2d0v5ov4fdw2gdJqkp6jSiW8dr-5tD6OyUFJ8R9IkP4Ac6JPDEM7qtK_6dBdeQaWRS749i04PmWWi9mfuGQ=w1280',
+        image : 'https://i.ibb.co/2gYRHBT/todoapp.png',
         projectTitle:'Todo App with React',
         projectTools:'HTML,CSS,React',
         liveDemo:'https://todo-list-app-react.vercel.app/',
@@ -73,7 +73,7 @@ const Projectdata = [
     },
     {
         id:10,
-        image : 'https://lh3.googleusercontent.com/BMeqUDdvb-G1URDTSJuV_DnxVc7AcX6zlQfBejcJZ9E4eZiFaefu0tgMmLC2RIJKA_OsFowigT8z8Zg95PAUSlj5lWQwdRf-_0VIa3wh6JHd3GxcGYJgraNq-1RlgOOR3Q=w1280',
+        image : 'https://i.ibb.co/SdYDw8j/faq.png',
         projectTitle:'FAQ',
         projectTools:'HTML,CSS,Javascript',
         liveDemo:'https://faq-vanilla-js.vercel.app/',
@@ -81,7 +81,7 @@ const Projectdata = [
     },
     {
         id:11,
-        image : 'https://lh6.googleusercontent.com/CyU_OCCTZb4ERm3NKQVOJmDZyMdy-39Ci8eipOKvSQGJVB5WtJ_2X-ezC6WlXuoLPLZjqC62wvAoY1SdvbjTA4eG5a-xtQ3EopDiuKmDr_MMF0NkmIv0mC2kv0v1Y5lpBA=w1280',
+        image : 'https://i.ibb.co/3YCfRFZ/imggallery.png',
         projectTitle:'Image Gallery and LightBox',
         projectTools:'HTML,CSS,Javascript',
         liveDemo:'https://abtahi-image-gallery.vercel.app/',
@@ -89,7 +89,7 @@ const Projectdata = [
     },
     {
         id:12,
-        image : 'https://lh3.googleusercontent.com/bBNxRPrhZUE3xwN4RQTbNHLZ2ifP_-hsuq2npvGldIUd1DRILRYuLI7FpE6817W3cPwn6YEQlIFgKVUfh404lvcAWLIcQfw84-dtJtQjaicg7APEAUBYoJRfRdtGPXC4SQ=w1280',
+        image : 'https://i.ibb.co/347Lxbn/github.png',
         projectTitle:'Search any Github Profiles',
         projectTools:'HTML,CSS,Javascript,API',
         liveDemo:'https://search-a-github-user.vercel.app/',
